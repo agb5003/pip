3d9aefa335961ccbcc8f53c527bc132c15a5a623
Maximilian Fernaldy <maximilian.fernaldy@gmail.com>
2023-11-11 00:45:07 +0900

    a

--- lec05/ex5-1/calc_func.c
+++ lec05/ex5-1/calc_func.c
@@ -4,7 +4,19 @@
 
 #include <stdio.h>
 
-/* User-defined add_func */
+// Function declarations
+float add_func(float x, float y);
+float sub_func(float a, float b);
+float multi_func(float a, float b);
+float div_func(float a, float b);
+
+int main() {
+    float result = sub_func(div_func(multi_func(add_func(2.0, 1.0), 6.0), 1.5), 3.0);
+    printf("%f\n", result);
+
+    return 0;
+}
+
 float add_func(float x, float y) {
     return x + y;
 }
@@ -20,12 +32,3 @@ float multi_func(float a, float b) {
 float div_func(float a, float b) {
     return a / b;
 }
-
-int main() {
-    float result = sub_func(div_func(multi_func(add_func(2.0, 1.0), 6.0), 1.5), 3.0);
-    printf("%f\n", result);
-
-    return 0;
-}
-
-
